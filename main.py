fd4877e70488000c65da17be6c9fafa3ef6aff9b
rezzraj <akshitaviraj@gmail.com>
2025-07-27 12:00:20 +0530

    plotting

--- main.py
+++ main.py
@@ -2,14 +2,35 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+
 
 #  Load the data
 iris = load_iris()
-X = iris.data  # features (the 4 numbers)
+
+df= pd.DataFrame(iris.data, columns=iris.feature_names)
+df['species']=iris.target
+df['species']=df['species'].apply(lambda x: iris.target_names[x])
+
+
+
+#counting the species
+sns.countplot(df, x='species')
+plt.show()
+
+
+#pairplotting
+sns.pairplot(df, hue='species')
+plt.show()
+x = iris.data  # features (the 4 numbers)
 y = iris.target  # labels (0,1,2)
 
 #  Split data- 80% train, 20% test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 #  Training model (K-Nearest Neighbors)
 model = KNeighborsClassifier(n_neighbors=3)
@@ -21,7 +42,7 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f" Accuracy: {accuracy * 100:.2f}%")
 
 #  Predict new input
-example = [[5.1, 3.5, 1.4, 0.2]]  # you can change these numbers
+example = [[1, 3.5, 1.4, 299]]  # you can change these numbers
 predicted_class = model.predict(example)[0]
 species = iris.target_names[predicted_class]
 print(f"Predicted species: {species}")
